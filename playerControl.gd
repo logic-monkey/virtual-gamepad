5ecbbc94c0399da91f866faa549ffe8ae333c35b
Logic Monkey <logic.monkey@protonmail.com>
2023-12-28 04:13:51 -0600

    Generalized Virtual Gamepad.

--- playerControl.gd
+++ playerControl.gd
@@ -3,6 +3,7 @@ extends Node
 
 var vc: VirtualGamepad
 @export var active := true
+@export var player := "p1"
 func _process(delta):
 	if Engine.is_editor_hint(): return
 	if has_node("/root/_IMP"):
@@ -15,5 +16,9 @@ func _process(delta):
 				break
 	if not vc: return
 	for button in vc.buttons:
-		vc.buttons[button].update_button(Input.is_action_pressed(button))
-	vc.stick = Input.get_vector("vp_left","vp_right","vp_up","vp_down")
+		vc.buttons[button].update_button(Input.is_action_pressed("%s_%s" % [player, button]))
+	vc.stick = Input.get_vector(
+				"%s_left" % player,
+				"%s_right" % player,
+				"%s_up" % player,
+				"%s_down" % player)
