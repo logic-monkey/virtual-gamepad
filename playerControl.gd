7593dd5d79705df8d794056a40e68b97c9f3acdd
Logic Monkey <logic.monkey@protonmail.com>
2024-01-02 06:05:47 -0600

    Added key to action map.

--- playerControl.gd
+++ playerControl.gd
@@ -4,6 +4,7 @@ extends Node
 var vc: VirtualGamepad
 @export var active := true
 @export var player := "p1"
+@export var button_map := {"jump":"j", "attack": ["a","b"], "run":"shift"}
 func _process(delta):
 	if Engine.is_editor_hint(): return
 	if has_node("/root/_IMP"):
@@ -15,8 +16,15 @@ func _process(delta):
 				vc = node
 				break
 	if not vc: return
-	for button in vc.buttons:
-		vc.buttons[button].update_button(Input.is_action_pressed("%s_%s" % [player, button]))
+	for button in button_map:
+		if not button in vc.buttons: continue
+		if button_map[button] is String:
+			vc.buttons[button].update_button(Input.is_action_pressed("%s_%s" % [player, button_map[button]]))
+		elif button_map[button] is Array:
+			var b := false
+			for sub_button in button_map[button]:
+				b = b or Input.is_action_pressed("%s_%s" % [player, sub_button])
+			vc.buttons[button].update_button(b)
 	vc.stick = Input.get_vector(
 				"%s_left" % player,
 				"%s_right" % player,
